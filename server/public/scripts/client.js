c912677b1168cbd8cf31a2dabc48fcc2ab6037d4
EmilyH <EmilyHHLam@gmail.com>
2017-04-02 13:38:18 -0500

    UI the listing

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,28 @@
+var current = 100;
+pageSize = 10;
+
 $(document).ready(function() {
   console.log("jquery source:");
   getListing();
+  var listing = {};
+  $('.submitaListing').on('click', function() {
+    listing.address = $("#address").val();
+    listing.city = $("#city").val();
+    listing.sqft = $("#sqft").val();
+    listing.price = $("#price").val();
+    listing.status = $("input[name=status]:checked").val();
+    console.log("address=" + listing.address);
+    console.log("city=" + listing.city);
+    console.log("sqft" + listing.sqft);
+    console.log("price = " + listing.price);
+    console.log("status=" + listing.status);
+
+    // $("#empName").val("");
+    // $("#empPosition").val("");
+    // $("#empSalary").val("");
+    postListing(listing);
+
+  });
 
 });
 function getListing() {
@@ -8,11 +30,23 @@ function getListing() {
     type: "GET",
     url: "/listing",
     success: function(response){
-    console.log("response" + response);
+    //console.log("response" + response);
     appendListings(response);
   }
   });
 }
+function postListing(data) {
+  console.log("post a listing");
+   $.ajax({
+     type:"POST",
+     url:"/listing",
+     data: data,
+     success: function(response){
+      getListing(response);
+    }
+  });
+}
+
 
 function appendListings(listings){
   for (var i=0; i<listings.length; i++){
@@ -20,29 +54,49 @@ function appendListings(listings){
     appendListing(listing);
   }
 }
+function convertCurrency(value) {
+  Price = '$' + value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  return Price;
+}
 
 function appendListing(listing) {
-  console.log("listing list here");
-  $(".listingContainer").append("<div class='well col-md-4 item'></div>");
-  var $el = $(".listingContainer").children().last();
-  if (listing.cost) {
-    $el.append("<p>" + listing.cost + "</p>");
-  } else {
+  showPage(1);
 
-    $el.append("<p>" + listing.rent + "</p>");
-  }
-  $el.append("<p>" + listing.address + "</p>");
-  $el.append("<p>" + listing.city + "</p>");
-  $el.append("<p>$" + listing.sqft + "</p>");
-  // if(employee.status){
-  //   $el.append("<p>Status: Current Employee");
-  // } else {
-  //   $el.append("<p>Status: Past Employee");
+  $(".listingContainer").append("<div class='listing well col-xs-5 col-md-3'></div>");
+  var $el = $(".listingContainer").children().last();
+  // if (listing.cost) {
+  // $el.append("<div id='forSaleImage'></div>");
   // }
-  // $el.append("<button data-id=" + employee._id +
-  //            " data-status=" + employee.status +
-  //            " class='update-btn btn btn-info'>Update Employee</button>");
-  //
-  // $el.append("<button data-id=" + employee._id +
-            // " class='delete-btn btn btn-danger'>Delete Employee</but
+  //$el.append("<div></div>");
+  //$el.append($(this).children().last());
+
+  if (listing.cost) {
+    $el.append("<div id='forSaleImage'></div>");
+    $el.append("<br><b>Price:</b> " + convertCurrency(listing.cost));
+  } else {
+    $el.append("<div id='forRentImage'></div>");
+    $el.append("<br><b>Price:</b>" + convertCurrency(listing.rent));
+  }
+  $el.append("<br><b>ID: </b>" + current++);
+    $el.append("<br><i>Total Sqft:</i>" + listing.sqft);
+  $el.append("<br>" + listing.address );
+  $el.append("<br>" + listing.city + ", MN</p>");
+
+
 }
+
+function showPage(page) {
+  //  $(".listing").hide();
+    $(".listing").each(function(n) {
+        if (n >= pageSize * (page - 1) && n < pageSize * page)
+            $(this).show();
+    });
+}
+
+
+
+$("#pagin li a").click(function() {
+    $("#pagin li a").removeClass("current");
+    $(this).addClass("current");
+    showPage(parseInt($(this).text()));
+});
--- server/routes/listings.js
+++ server/routes/listings.js
@@ -11,39 +11,8 @@ var ListingSchema = mongoose.Schema({
 });
 var Listings = mongoose.model("listings", ListingSchema);
 
-//var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
-// Defines HOW Documents will be saved to the Database
-// var Listing = mongoose.Schema({
-//   address: String,
-//   city: String,
-//   sqft: Number,
-//   status: String
-// });
+//GET listing
 
-// var Status = new Schema({
-//
-//     cost: Number,
-//     rent: Number
-//
-// });
-
-//var listing = mongoose.model('Listing', Listing, 'listings');
-// var Status = mongoose.model( 'Status', Status, 'listings');
-//
-// Listing.status = mongoose.model('Status', Status, 'listings');
-
-//GET employees`
-//router.get("/status", function(req,res){
-  //Get all employees
-  // console.log("inside status" + Status);
-  // Status.find(function(err, saleList){
-  //   console.log('sold list ' + saleList);
-  //   if(err){
-  //     console.log(err);
-  //     res.sendStatus(500);
-  //   }
-  //   res.send(saleList);
-  // });
   router.get("/", function(req, res){
     Listings.find(function(err, Listings){
       //Get all employees
@@ -57,5 +26,28 @@ var Listings = mongoose.model("listings", ListingSchema);
 
 
   });
+  //Save a new employee
+router.post("/", function(req,res){
+  //Instance of the Model to be saved to the database
+  var listing = new Listings();
+  var status = req.body.status;
+  console.log("status=" + status);
+  if (status == 0) {
+    listing.rent = req.body.price;
+  }else {
+    listing.cost = req.body.price;
+  }
+  listing.address = req.body.address;
+  listing.city = req.body.city;
+  listing.sqft = req.body.sqft;
+  listing.save(function(err, savedListing){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+
+    res.send(savedListing);
+  });
+});
 
 module.exports = router;
